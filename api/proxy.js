c2ec0c66e4812a6513b3c1b4c3491d3218a1a584
Zhou Kaijin <76028864@qq.com>
2025-02-10 20:56:09 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://www.att.com/deviceunlock/";//your website url
+  let target = "https://www.att.com/deviceunlock/unlockstep1/";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
